b31c0a0e8736f278544f2cc98a11c6c9981e1a3f
Omar Moreno <email@omarmoreno.net>
2020-08-26 16:50:09 -0700

    Move initialization to Interactive class.

--- src/fire_interactive.cxx
+++ src/fire_interactive.cxx
@@ -1,14 +1,24 @@
 
+#include <memory>
 #include <stdio.h>
 
-#include "G4UIExecutive.hh"
+/*~~~~~~~~~~~~~*/
+/*   SimCore   */
+/*~~~~~~~~~~~~~*/
+#include "SimCore/Interactive.h" 
+
+using namespace ldmx; 
 
 int main(int argc, char** argv) {
 
-    // Start an interactive session
-    auto ui{new G4UIExecutive(argc, argv)};
-    ui->SessionStart(); 
-
-    delete ui; 
-
+    // If a python configuration has been passed, configure the interactive 
+    // sesssion before starting. Otherwise, just start the session.
+    std::unique_ptr< Interactive > session{nullptr}; 
+    if (argc == 1) {
+        session = std::make_unique< Interactive >(); 
+    } else {
+    } 
+        
+    // Start the interactive session
+    session->startSession(argc, argv); 
 }
