3d9c1adfe13a06cd49ef49d1b46b2767c0816ee6
Wesley Ketchum <wesketchum@gmail.com>
2022-11-08 16:50:30 +0100

    initial commit of a GenieGenerator. runs one event before it crashes...

---
+++ include/SimCore/Generators/GenieGenerator.h
@@ -0,0 +1,93 @@
+/**
+ * @file GenieGenerator.h
+ * @brief Simple GENIE event generator.
+ * @author Wesley Ketchum, FNAL
+ */
+
+#ifndef SIMCORE_GENIE_GENERATOR_H
+#define SIMCORE_GENIE_GENERATOR_H
+
+//------------//
+//   GENIE   //
+//------------//
+#include "GENIE/Framework/EventGen/GEVGDriver.h"
+
+//------------//
+//   LDMX     //
+//------------//
+#include "SimCore/PrimaryGenerator.h"
+
+#include <vector>
+#include <string>
+
+// Forward declarations
+class G4Event;
+
+namespace simcore {
+namespace generators {
+
+/**
+ * @class GenieGenerator
+ * @brief Class that uses GENIE's GEVGDriver to generator eN interactions.
+ */
+class GenieGenerator : public simcore::PrimaryGenerator {
+ public:
+  /**
+   * Constructor.
+   *
+   * @param parameters Parameters used to configure GENIE generator.
+   *
+   * Parameters:
+   *  verbosity : > 1 means print configuration
+   *  energy    : energy of initial electron (GeV)
+   *  targets   : list of ten-digit 10LZZZAAAI target codes
+   *  abundances: list of relative abundances for the given targets
+   *  position  : position of interaction from (mm three-vector)
+   *  time      : time to shoot at (ns)
+   *  direction : direction to shoot in (unitless three-vector)
+   *  tune      : name of GENIE tune
+   */
+  GenieGenerator(const std::string& name, const framework::config::Parameters& parameters);
+
+  /// Destructor
+  ~GenieGenerator();
+
+  /**
+   * Generate the primary vertices in the Geant4 event.
+   *
+   * @param event The Geant4 event.
+   */
+  void GeneratePrimaryVertex(G4Event* event) final override;
+
+  void RecordConfig(const std::string& id, ldmx::RunHeader& rh) final override;
+
+ private:
+  /**
+   * The GENIE event generator driver
+   */
+  genie::GEVGDriver evg_driver_;
+
+  int verbosity_;
+  double energy_;
+  std::vector<int> targets_;
+  std::vector<double> abundances_;
+  std::vector<double> position_;
+  double time_;
+  std::vector<double> direction_;
+
+  std::string tune_;
+  std::string spline_file_;
+
+  
+  
+  void fillConfig(const framework::config::Parameters&);     ///fill the configuration
+  bool validateConfig(); ///simple validation check on configuration params
+
+  void initializeGENIE(); ///GENIE initialization
+  
+};  // ParticleGun
+
+}  // namespace generators
+}  // namespace simcore
+
+#endif  // SIMCORE_PARTICLE_GUN_H
---
+++ src/SimCore/Generators/GenieGenerator.cxx
@@ -0,0 +1,250 @@
+/**
+ * @file GenieGenerator.cxx
+ * @brief Simple GENIE event generator.
+ * @author Wesley Ketchum, FNAL
+ */
+
+#include "SimCore/Generators/GenieGenerator.h"
+#include "SimCore/UserPrimaryParticleInformation.h"
+
+// GENIE
+#include "Framework/Utils/AppInit.h"
+#include "GENIE/Framework/Utils/RunOpt.h"
+#include "Framework/Utils/XSecSplineList.h"
+#include "GENIE/Framework/Interaction/InitialState.h"
+#include "Framework/EventGen/EventRecord.h"
+#include "Framework/GHEP/GHepParticle.h"
+
+
+#include "Framework/Conventions/XmlParserStatus.h"
+#include "Framework/Conventions/GBuild.h"
+#include "Framework/Conventions/Controls.h"
+#include "Framework/EventGen/EventRecord.h"
+#include "Framework/EventGen/GFluxI.h"
+#include "Framework/EventGen/GEVGDriver.h"
+#include "Framework/EventGen/GMCJDriver.h"
+#include "Framework/EventGen/GMCJMonitor.h"
+#include "Framework/Interaction/Interaction.h"
+#include "Framework/Messenger/Messenger.h"
+#include "Framework/Ntuple/NtpWriter.h"
+#include "Framework/Ntuple/NtpMCFormat.h"
+#include "Framework/Numerical/RandomGen.h"
+#include "Framework/Numerical/Spline.h"
+#include "Framework/ParticleData/PDGCodes.h"
+#include "Framework/ParticleData/PDGUtils.h"
+#include "Framework/Utils/AppInit.h"
+#include "Framework/Utils/RunOpt.h"
+#include "Framework/Utils/XSecSplineList.h"
+#include "Framework/Utils/StringUtils.h"
+#include "Framework/Utils/PrintUtils.h"
+#include "Framework/Utils/SystemUtils.h"
+#include "Framework/Utils/CmdLnArgParser.h"
+
+
+// Geant4
+#include "G4Event.hh"
+
+// ROOT
+#include <TParticle.h>
+#include <TVector3.h>
+#include <TLorentzVector.h>
+
+// standard
+#include <iostream>
+#include <sstream>
+
+namespace simcore {
+namespace generators {
+
+void GenieGenerator::fillConfig(const framework::config::Parameters& p)
+{
+
+  energy_      = p.getParameter<double>("energy");// * GeV;
+
+  targets_     = p.getParameter< std::vector<int> >("targets");
+  abundances_  = p.getParameter< std::vector<double> >("abundances");
+
+  time_        = p.getParameter<double>("time");// * ns;
+  position_    = p.getParameter< std::vector<double> >("position");
+  direction_   = p.getParameter< std::vector<double> >("direction");
+
+  tune_        = p.getParameter<std::string>("tune");
+  spline_file_ = p.getParameter<std::string>("spline_file");
+
+
+  for(size_t i=0; i<position_.size(); ++i)
+    position_[i] = position_[i];// * mm;
+}
+
+bool GenieGenerator::validateConfig()
+{
+  bool ret=true;
+  
+  if (targets_.size()==0 || abundances_.size()==0)
+    {
+      std::cout << "targets and/or abundances sizes are zero."
+		<< "  " << targets_.size() << ", " << abundances_.size()
+		<< std::endl;
+      ret=false;
+    }
+  if (targets_.size() != abundances_.size())
+    {
+      std::cout << "targets and abundances sizes unequal."
+		<< "  " << targets_.size() << " != " << abundances_.size()
+		<< std::endl;
+      ret=false;
+    }
+
+  if (position_.size()!=3 || direction_.size()!=3)
+    {
+      std::cout << "position and/or direction sizes are not 3."
+		<< "  " << position_.size() << ", " << direction_.size()
+		<< std::endl;
+      ret=false;
+    }
+
+
+  //normalize abundances
+  float abundance_sum=0;
+  for( auto a : abundances_)
+    abundance_sum+=a;
+
+  if( std::abs(abundance_sum)<1e-6)
+    {
+      std::cout << "abundances list sums to zero? " << abundance_sum << std::endl;
+      ret=false;
+    }
+  
+  for(size_t i_a; i_a<abundances_.size(); ++i_a)
+    abundances_[i_a] = abundances_[i_a]/abundance_sum;
+
+
+  double dir_total_sq=0;
+  for( auto d : direction_)
+    dir_total_sq += d*d;
+  
+  if( dir_total_sq<1e-6)
+    {
+      std::cout << "direction vector is zero or negative? "
+		<< "(" << direction_[0] << "," << direction_[1] << "," << direction_[2] << ")"
+		<< std::endl;
+      ret=false;
+    }
+  for(size_t i_d; i_d<direction_.size(); ++i_d)
+    direction_[i_d] = direction_[i_d]/std::sqrt(dir_total_sq);
+  
+  
+  return ret;
+}
+
+void GenieGenerator::initializeGENIE()
+{
+  genie::RunOpt::Instance()->SetTuneName(tune_);
+  if ( ! genie::RunOpt::Instance()->Tune() ) {
+    EXCEPTION_RAISE("ConfigurationException","No TuneId in RunOption.");
+  }
+  genie::RunOpt::Instance()->BuildTune();
+
+  genie::utils::app_init::XSecTable(spline_file_, false);
+  genie::GHepRecord::SetPrintLevel(1);
+
+  evg_driver_.SetEventGeneratorList(genie::RunOpt::Instance()->EventGeneratorList());
+  evg_driver_.SetUnphysEventMask(*genie::RunOpt::Instance()->UnphysEventMask());
+  
+}
+
+  
+GenieGenerator::GenieGenerator(const std::string& name,
+			       const framework::config::Parameters& p)
+    : PrimaryGenerator(name, p)
+{
+
+  std::cout << "Constructing GenieGenerator" << std::endl;
+  
+  fillConfig(p);
+
+  if(!validateConfig())
+    EXCEPTION_RAISE("ConfigurationException","Configuration not valid.");
+
+  initializeGENIE();
+  
+}
+
+GenieGenerator::~GenieGenerator() {}
+  
+void GenieGenerator::GeneratePrimaryVertex(G4Event* event)
+{
+  std::cout << "Starting event ..." << std::endl;
+  
+  //for now, just the first. later picking a target from list
+  genie::InitialState initial_state(targets_.front(),11);
+  evg_driver_.Configure(initial_state);
+
+  //setup the initial election
+  TParticle initial_e;
+  initial_e.SetPdgCode(11);
+  auto elec_i_p = std::sqrt(energy_*energy_ - initial_e.GetMass()*initial_e.GetMass());
+  initial_e.SetMomentum(elec_i_p*direction_[0],
+			elec_i_p*direction_[1],
+			elec_i_p*direction_[2],
+			energy_);
+  TLorentzVector e_p4;
+  initial_e.Momentum(e_p4);
+
+  std::cout << "Run GENIE ..." << std::endl;
+  
+  //GENIE magic
+  genie::EventRecord *genie_event = evg_driver_.GenerateEvent(e_p4);
+
+  std::cout << "GENIE done..." << std::endl;
+
+  //setup the primary vertex now
+  G4PrimaryVertex* vertex = new G4PrimaryVertex();
+  vertex->SetPosition(position_[0],
+		      position_[1],
+		      position_[2]);
+  vertex->SetWeight(genie_event->Weight());
+  
+  //loop over the entries and add to the G4Event
+  int nentries = genie_event->GetEntries();
+
+  for(int i_p=0; i_p<nentries; ++i_p)
+    {
+      genie::GHepParticle *p = (genie::GHepParticle*)(*genie_event)[i_p];
+
+      //make sure it's a final state particle
+      if (p->Status()!=1)
+	continue;
+
+      std::cout << "Adding particle " << p->Pdg() << " with status " << p->Status() << " energy " << p->E() << " ..." << std::endl;
+
+      G4PrimaryParticle* primary = new G4PrimaryParticle();
+      primary->SetPDGcode(p->Pdg());
+      primary->Set4Momentum(p->Px(), p->Py(), p->Pz(), p->E());
+      primary->SetProperTime(time_);
+
+      UserPrimaryParticleInformation* primaryInfo =
+	new UserPrimaryParticleInformation();
+      primaryInfo->setHepEvtStatus(1);
+      primary->SetUserInformation(primaryInfo);
+      
+      vertex->SetPrimary(primary);
+    }
+
+  std::cout << "Add vertex to event." << std::endl;
+  
+  //add the vertex to the event
+  event->AddPrimaryVertex(vertex);
+
+  std::cout << "Finished?" << std::endl;
+  
+}
+
+void GenieGenerator::RecordConfig(const std::string& id, ldmx::RunHeader& rh) {
+  rh.setStringParameter(id+" Class", "simcore::generators::GenieGenerator");
+}
+
+}  // namespace generators
+}  // namespace simcore
+
+DECLARE_GENERATOR(simcore::generators::GenieGenerator)
